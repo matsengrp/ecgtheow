093b84e26768183d12169fcf1ed7c090c90914a0
Amrit Dhar <adhar@uw.edu>
2017-07-13 20:03:38 -0700

    annotated graphviz plotting

--- python/trees_to_counted_ancestors.py
+++ python/trees_to_counted_ancestors.py
@@ -2,6 +2,8 @@
 
 import argparse
 from collections import Counter
+import math
+import numpy as np
 from collections import OrderedDict
 import os
 import sys
@@ -11,6 +13,19 @@ from Bio.Alphabet import IUPAC
 from Bio.Seq import Seq
 import dendropy
 import graphviz
+
+from tabulate_mutations import find_muts
+
+
+def format_label(label):
+    '''
+    Format the graphviz edge label to appear "rectangular".
+    '''
+    label_space = int(np.sqrt(len(label)))
+    for i in np.arange(label_space, len(label), label_space + 1):
+        label.insert(i, "\n")
+
+    return label
 
 
 def seqs_of_tree(t, seed):
@@ -89,6 +104,9 @@ if __name__ == '__main__':
         # Note flipped below because we want to go from ancestor to descendant.
         edge_c.update((w, v) for v, w in zip(l[:-1], l[1:]))
 
+    # Count the number of BEAST trees.
+    num_trees = node_c.most_common(1)[0][1]
+
     if node_c.most_common() == []:
         raise Exception ("Nothing to count! Is your burnin too large?")
     out_seqs = OrderedDict()
@@ -116,6 +134,7 @@ if __name__ == '__main__':
             f.write('{}\n'.format(v))
 
     dot = graphviz.Digraph(comment=" ".join(sys.argv), format='png')
+    dot.attr(size='15,15', ratio='fill', fontsize='9')
 
 # Commented because it defeats our filtering mechanism below. Could re-add if
 # we want to add extra information to nodes.
@@ -124,7 +143,15 @@ if __name__ == '__main__':
 
     for ((a,b), count) in edge_c.most_common(None):
         if a != b and count >= args.filter:
-            dot.edge(seqs_out[a], seqs_out[b], label=str(count))
+            edge_conf = int(20 + (100-20) * float(count) / node_c[a])
+            dot.edge(seqs_out[a], seqs_out[b], xlabel=" ".join(format_label(find_muts(a, b))), color="#000000" + (str(edge_conf) if edge_conf < 100 else ""))
+
+            if seqs_out[a] != "naive0":
+                child_conf = int(10 + (100-10) * float(node_c[a]) / num_trees)
+                dot.node(seqs_out[a], style="filled", fillcolor="#ff0000" + (str(child_conf) if child_conf < 100 else ""))
+            if seqs_out[b] != args.seed:
+                child_conf = int(10 + (100-10) * float(node_c[b]) / num_trees)
+                dot.node(seqs_out[b], style="filled", fillcolor="#ff0000" + (str(child_conf) if child_conf < 100 else ""))
 
     dot.save(base+'.aa_lineage_graph.dot')
     dot.render(base+'.aa_lineage_graph')
