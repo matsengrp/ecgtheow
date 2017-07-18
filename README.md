fafbeb7ca7957bb1bb03156eeda86117f69fc1ea
Amrit Dhar <adhar@uw.edu>
2017-07-18 15:20:56 -0700

    BEAST XML template creation & rendering

--- README.md
+++ README.md
@@ -12,11 +12,24 @@ Please only commit changes to Jupyter/Sage notebooks after clearing output.
 
 # Example usage
 
-First pull the runs, etc.
+To generate a BEAST XML input file, run the following command:
+
+    python/generate_beast_xml_input.py --naive naive0 --seed BF520.1-igh templates/beast_template.xml data/2017-07-10/BF520.1-h-IgH.family_0.healthy.tre.seedpruned.100.ids.fasta
+
+Now, run BEAST using the previously created XML file:
+
+    cp -r beast/plugins ${BEAST_ROOT}
+    mv runs/2017-07-10/BF520.1-h-IgH.family_0.healthy.tre.seedpruned.100.ids.xml ${BEAST_ROOT}
+    cd ${BEAST_ROOT}
+    java -jar lib/beast.jar -seed 0 -overwrite BF520.1-h-IgH.family_0.healthy.tre.seedpruned.100.ids.xml
+    mv BF520.1-h-IgH.family_0.healthy.tre.seedpruned.100.ids.* ${ECGTHEOW_ROOT}/runs/2017-07-10
+    cd ${ECGTHEOW_ROOT}
+
+Alternatively, you can pull the runs from `stoat`.
 
     ./pull-ignored.sh
 
-Then we process the BEAST runs into a graph and a list of sequences:
+Then, we process the BEAST runs into a graph and a list of sequences:
 
     python/trees_to_counted_ancestors.py --seed BF520.1-igh --burnin 1000 --filter 100 runs/2017-07-10/BF520.1-h-IgH.family_0.healthy.tre.seedpruned.100.ids.trees data/2017-07-10/BF520.1-h-IgH.family_0.healthy.tre.seedpruned.100.ids.fasta
 
---
+++ python/generate_beast_xml_input.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import argparse
+import jinja2
+import os
+import re
+from Bio import SeqIO
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Create a BEAST XML input file from a template.")
+    parser.add_argument(
+        'template_path', type=str,
+        help="Path to XML template.")
+    parser.add_argument(
+        'fasta_path', type=str,
+        help="Path to FASTA input alignment.")
+    parser.add_argument(
+        '--naive', type=str, required=True,
+        help="The name of the naive sequence.")
+    parser.add_argument(
+        '--seed', type=str, required=True,
+        help="The name of the seed sequence.")
+    parser.add_argument(
+        '--iter', type=int, default=10000000,
+        help="The number of total MCMC iterations.")
+    parser.add_argument(
+        '--thin', type=int, default=1000,
+        help="The MCMC sampling frequency.")
+
+    args = parser.parse_args()
+
+    fasta_path = args.fasta_path.lstrip("./")
+    _, date, filename = re.split("/", args.fasta_path)
+
+    id_seq = {
+        k:str(v.seq) for k,v in
+        SeqIO.to_dict(SeqIO.parse(args.fasta_path, "fasta")).items() }
+
+    assert args.naive in id_seq, "Sequence %r not found in FASTA file." % args.naive
+    assert args.seed in id_seq, "Sequence %r not found in FASTA file." % args.seed
+
+    temp_vars = dict(id_seq=id_seq,
+                     naive=args.naive,
+                     iter=args.iter,
+                     thin=args.thin,
+                     basename=os.path.splitext(filename)[0])
+
+    env = jinja2.Environment(loader = jinja2.FileSystemLoader('.'),
+                             undefined=jinja2.StrictUndefined,
+                             trim_blocks=True, lstrip_blocks=True)
+
+    xml_path = "runs/" + date + "/" + temp_vars["basename"] + ".xml"
+    env.get_template(args.template_path).stream(**temp_vars).dump(xml_path)
