4541ee8d1849a4894ec14d8464d319604804967c
Amrit Dhar <adhar@uw.edu>
2017-08-03 15:02:19 -0700

    modified commandline args

--- run_beast_asr.sh
+++ run_beast_asr.sh
@@ -34,6 +34,21 @@ do
       shift 2
       NARGS=$((NARGS-2))
       ;;
+    --cft-version)
+      CFT_VERSION="$2"
+      shift 2
+      NARGS=$((NARGS-2))
+      ;;
+    --chain)
+      CHAIN="$2"
+      shift 2
+      NARGS=$((NARGS-2))
+      ;;
+    --isotype)
+      ISOTYPE="$2"
+      shift 2
+      NARGS=$((NARGS-2))
+      ;;
     --nprune)
       NPRUNE="$2"
       shift 2
@@ -66,43 +81,48 @@ do
   esac
 done
 
+if [ -z "${CFT_VERSION}" ]; then CFT_VERSION="v9"; fi
+if [ -z "${CHAIN}" ]; then CHAIN="h"; fi
+if [ -z "${ISOTYPE}" ]; then ISOTYPE="G"; fi
 if [ -z "${NPRUNE}" ]; then NPRUNE=100; fi
 if [ -z "${MCMC_ITER}" ]; then MCMC_ITER=10000000; fi
 if [ -z "${MCMC_THIN}" ]; then MCMC_THIN=1000; fi
 if [ -z "${MCMC_BURNIN}" ]; then MCMC_BURNIN=1000; fi
 if [ -z "${ASR_NFILTER}" ]; then ASR_NFILTER=100; fi
 
-if [ -z "${NAIVE}" ] || [ -z "${SEED}" ] || [ -z "${BEAST_DIR}" ] || [ -z "${BEAGLE_DIR}" ]
-  then
-    echo "ERROR: Please specify the '--naive', '--seed', '--beast-dir', and '--beagle-dir' arguments."
-    exit 1
-fi
+FAIL=0
+if [ -z "${NAIVE}" ]; then echo "ERROR: Please specify the '--naive' command line argument."; FAIL=1; fi
+if [ -z "${SEED}" ]; then echo "ERROR: Please specify the '--seed' command line argument."; FAIL=1; fi
+if [ -z "${BEAST_DIR}" ]; then echo "ERROR: Please specify the '--beast-dir' command line argument."; FAIL=1; fi
+if [ -z "${BEAGLE_DIR}" ]; then echo "ERROR: Please specify the '--beagle-dir' command line argument."; FAIL=1; fi
+if [ "${FAIL}" -eq 1 ]; then exit 1; fi
+
 
 # Make the data and runs directories.
 mkdir -p data runs
 
 # Grab the sequences from stoat.
-scp stoat:/fh/fast/matsen_e/processed-data/partis/laura-mb/v9/seeds/${SEED}/BF520-h-IgG/run-viterbi-best-plus-0.csv data/${SEED}.csv
+scp stoat:/fh/fast/matsen_e/processed-data/partis/laura-mb/v9/seeds/${SEED}-ig${CHAIN}/${SEED%%.*}-${CHAIN}-Ig${ISOTYPE}/run-viterbi-best-plus-0.csv data/${SEED}-ig${CHAIN}.csv
 
 # Create the unpruned FASTA file (containing both the naive and seed sequences).
-lib/pandis/transpose_family.py data/${SEED}.csv
-lib/pandis/healthy_to_fasta.py data/${SEED}.family_0.csv
-lib/pandis/get_naives.py data/${SEED}.csv >> data/${SEED}.family_0.healthy.fasta
+lib/pandis/transpose_family.py data/${SEED}-ig${CHAIN}.csv
+lib/pandis/healthy_to_fasta.py data/${SEED}-ig${CHAIN}.family_0.csv
+lib/pandis/get_naives.py data/${SEED}-ig${CHAIN}.csv >> data/${SEED}-ig${CHAIN}.family_0.healthy.fasta
 
 # Generate a tree and prune sequences from the clonal family.
-FastTree -nt data/${SEED}.family_0.healthy.fasta > data/${SEED}.family_0.healthy.tre
-lib/cft/bin/prune.py --naive ${NAIVE} --seed ${SEED} data/${SEED}.family_0.healthy.tre -n ${NPRUNE} --strategy seed_lineage > data/${SEED}.family_0.healthy.seedpruned.${NPRUNE}.ids
-seqmagick convert --include-from-file data/${SEED}.family_0.healthy.seedpruned.${NPRUNE}.ids data/${SEED}.family_0.healthy.fasta data/${SEED}.family_0.healthy.seedpruned.${NPRUNE}.fasta
+FastTree -nt data/${SEED}-ig${CHAIN}.family_0.healthy.fasta > data/${SEED}-ig${CHAIN}.family_0.healthy.tre
+lib/cft/bin/prune.py --naive ${NAIVE} --seed ${SEED}-ig${CHAIN} data/${SEED}-ig${CHAIN}.family_0.healthy.tre -n ${NPRUNE} --strategy seed_lineage > data/${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.ids
+seqmagick convert --include-from-file data/${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.ids data/${SEED}-ig${CHAIN}.family_0.healthy.fasta data/${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.fasta
 
 # Construct the BEAST XML input file.
-python/generate_beast_xml_input.py --naive ${NAIVE} --seed ${SEED} templates/beast_template.xml data/${SEED}.family_0.healthy.seedpruned.${NPRUNE}.fasta --iter ${MCMC_ITER} --thin ${MCMC_THIN}
+python/generate_beast_xml_input.py --naive ${NAIVE} --seed ${SEED}-ig${CHAIN} templates/beast_template.xml data/${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.fasta --iter ${MCMC_ITER} --thin ${MCMC_THIN}
 
 # Run BEAST.
-java -Xms64m -Xmx2048m -Djava.library.path=${BEAGLE_DIR} -Dbeast.plugins.dir=beast/plugins -jar ${BEAST_DIR}/lib/beast.jar -warnings -seed 1 -overwrite runs/${SEED}.family_0.healthy.seedpruned.${NPRUNE}.xml
-if [ -e "${SEED}.family_0.healthy.seedpruned.${NPRUNE}.log" ]
+java -Xms64m -Xmx2048m -Djava.library.path=${BEAGLE_DIR} -Dbeast.plugins.dir=beast/plugins -jar ${BEAST_DIR}/lib/beast.jar -warnings -seed 1 -overwrite runs/${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.xml
+if [ -e "${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.log" ]
   then
-    mv ${SEED}.family_0.healthy.seedpruned.${NPRUNE}.* runs/
+    mv ${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.* runs/
 fi
 
 # Summarize the BEAST results.
-python/trees_to_counted_ancestors.py --seed ${SEED} --burnin ${MCMC_BURNIN} --filter ${ASR_NFILTER} runs/${SEED}.family_0.healthy.seedpruned.${NPRUNE}.trees data/${SEED}.family_0.healthy.seedpruned.${NPRUNE}.fasta
+python/trees_to_counted_ancestors.py --seed ${SEED}-ig${CHAIN} --burnin ${MCMC_BURNIN} --filter ${ASR_NFILTER} runs/${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.trees data/${SEED}-ig${CHAIN}.family_0.healthy.seedpruned.${NPRUNE}.fasta
