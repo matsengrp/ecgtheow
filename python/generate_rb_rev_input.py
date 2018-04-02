c1e5c91a66680500285e7dce57fed237b61642dd
Amrit Dhar <adhar@uw.edu>
2018-04-02 09:28:31 -0700

    added jinja revbayes templater

---
+++ python/generate_rb_rev_input.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import argparse
+import jinja2
+import os
+import re
+
+from util_functions import parse_fasta_seqs
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Create a RevBayes Rev input file from a template.")
+    parser.add_argument(
+        'template_path', type=str,
+        help="Path to Rev template.")
+    parser.add_argument(
+        'fasta_path', type=str,
+        help="Path to FASTA input alignment.")
+    parser.add_argument(
+        '--naive', type=str, required=True,
+        help="The name of the naive sequence.")
+    parser.add_argument(
+        '--seed', type=str, required=True,
+        help="The name of the seed sequence.")
+    parser.add_argument(
+        '--iter', type=int, required=True,
+        help="The number of total MCMC iterations.")
+    parser.add_argument(
+        '--thin', type=int, required=True,
+        help="The MCMC sampling frequency.")
+    parser.add_argument(
+        '--rev-path',
+        help="The Rev output file path.")
+
+    args = parser.parse_args()
+
+    args.fasta_path = args.fasta_path.lstrip("./")
+    filename = re.split("/", args.fasta_path)[-1]
+
+    id_seq = parse_fasta_seqs(args.fasta_path)
+
+    assert args.naive in id_seq, "Sequence %r not found in FASTA file." % args.naive
+    assert args.seed in id_seq, "Sequence %r not found in FASTA file." % args.seed
+
+    temp_vars = dict(
+        fasta_path=args.fasta_path,
+        naive=args.naive,
+        iter=args.iter,
+        thin=args.thin,
+        basename=os.path.splitext(filename)[0]
+    )
+
+    env = jinja2.Environment(loader = jinja2.FileSystemLoader('.'),
+                             undefined=jinja2.StrictUndefined,
+                             trim_blocks=True, lstrip_blocks=True)
+
+    rev_path = args.rev_path or ("runs/" + temp_vars["basename"] + ".rev")
+    env.get_template(args.template_path).stream(**temp_vars).dump(rev_path)
